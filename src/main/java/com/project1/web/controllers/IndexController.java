f15eb75a9256889b0b2aa6eb26d7ac733cfc9099
Aoran <zhengaoran33@gmail.com>
2018-03-24 20:34:58 -0700

    Added placeholder

--- src/main/java/com/project1/web/controllers/IndexController.java
+++ src/main/java/com/project1/web/controllers/IndexController.java
@@ -1,13 +1,12 @@
-package com.project1.controllers;
+package com.project1.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloWorldController {
-
+public class IndexController {
 	@RequestMapping(value="/")
-	public String welcome() {
-		return "welcome";
+	public String home() {
+		return "index";
 	}
 }
